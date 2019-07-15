28e8df578aa0d2b3de3e6f920d2cc27a218cc909
Agus <agusgc_99@hotmail.com>
2019-07-15 11:04:14 -0300

    header fix y demas componentes

--- almendras/src/app/app-routing.module.ts
+++ almendras/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { GoogleMapComponent } from './google-map/google-map.component';
+import { RecetasComponent } from './recetas/recetas.component';
+import { ProductosComponent } from './productos/productos.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: BodyComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'mapa', component: GoogleMapComponent },
+  { path: 'recetas', component: RecetasComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- almendras/src/app/app.module.ts
+++ almendras/src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
+import { ProductosComponent } from './productos/productos.component';
+import { RecetasComponent } from './recetas/recetas.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { GoogleMapComponent } from './google-map/google-map.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    ProductosComponent,
+    RecetasComponent,
+    ContactoComponent,
+    GoogleMapComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
