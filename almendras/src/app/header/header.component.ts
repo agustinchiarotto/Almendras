81f9a234f452a4ae14151ad44701c0ae792665bd
Agus <agusgc_99@hotmail.com>
2019-07-23 20:28:10 -0300

    header con coloroes

--- almendras/src/app/header/header.component.ts
+++ almendras/src/app/header/header.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  prevScrollpos = window.pageYOffset;
+  constructor() { 
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
+
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
 
 }
