c235f4a5c9a0e3e1f5e03021e4be13f9da6739ae
Unknown <davidtorchinsky@gmail.com>
2019-08-06 17:50:33 -0300

    Intentando poner appi mapa

--- almendras/src/app/google-map/google-map.component.ts
+++ almendras/src/app/google-map/google-map.component.ts
@@ -11,5 +11,10 @@ export class GoogleMapComponent implements OnInit {
 
   ngOnInit() {
   }
+  
+
+  title: string = 'My first AGM project';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 
 }
