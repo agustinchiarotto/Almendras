08ec3ed9cfdd412aea10e65c6e727f8d25906567
GaboCancellieri <cancellieri.gabriel@gmail.com>
2019-08-06 13:22:54 -0300

    Productos
    Dulces - Salados - Naturales y Harinas

--- almendras/src/app/productos/productos.component.ts
+++ almendras/src/app/productos/productos.component.ts
@@ -9,6 +9,101 @@ export class ProductosComponent implements OnInit {
 
   constructor() { }
 
+  dulces = [{
+    nombre: 'ALMENDRAS',
+    negrita: 'CON CHOCOLATE',
+    imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+  },
+  {
+    nombre: 'PASAS DE UVA',
+    negrita: 'CON CHOCOLATE',
+    imagen: '../../assets/imagenes/caja_pasas_chocolate.png' 
+  },
+  {
+    nombre: 'AVELLANAS',
+    negrita: 'CON CHOCOLATE',
+    imagen: '../../assets/imagenes/caja_pasas_uva.png' 
+  },
+  {
+    nombre: 'ALMENDRAS',
+    negrita: 'CON CHOCOLATE',
+    imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+  },
+  {
+    nombre: 'ALMENDRAS',
+    negrita: 'CON CHOCOLATE',
+    imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+  }
+];
+
+salados = [{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},
+{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},
+];
+
+naturales = [{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},
+{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},
+];
+
+harinas = [{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},
+{
+  nombre: 'ALMENDRAS',
+  negrita: 'CON CHOCOLATE',
+  imagen: '../../assets/imagenes/caja_avellanas_chocolate.png' 
+},
+];
+
   ngOnInit() {
   }
 //arreglo con ngfor
