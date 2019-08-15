2331076db0e6c521ca936912115634d996800f87
agus <agusgc_99@hotmail.com>
2019-08-15 15:57:16 -0300

    agregado de imagenes de productos

--- almendras/src/app/productos/productos.component.ts
+++ almendras/src/app/productos/productos.component.ts
@@ -12,27 +12,32 @@ export class ProductosComponent implements OnInit {
   dulces = [{
     nombre: 'ALMENDRAS',
     negrita: 'CON CHOCOLATE',
-    imagen: '../../assets/imagenes/dulces_alm_choco.jpg' 
+    imagen: '../../assets/imagenes/productos/dulces_alm_choco.jpg' ,
+    size:false //para ver si es 100% o 50%
   },
   {
     nombre: 'PASAS DE UVA',
     negrita: 'CON CHOCOLATE',
-    imagen: '../../assets/imagenes/dulces_pasas_choco.jpg' 
+    imagen: '../../assets/imagenes/productos/dulces_pasas_choco.jpg',
+    size:false
   },
   {
     nombre: 'AVELLANAS',
     negrita: 'CON CHOCOLATE',
-    imagen: '../../assets/imagenes/dulces_avellana_choco.jpg' 
+    imagen: '../../assets/imagenes/productos/dulces_avellana_choco.jpg',
+    size:false 
   },
   {
     nombre: 'ALMENDRAS',
     negrita: 'DULCES',
-    imagen: '../../assets/imagenes/dulces_alm_dulces.jpg' 
+    imagen: '../../assets/imagenes/productos/dulces_alm_dulces.jpg',
+    size:true
   },
   {
     nombre: 'ALMENDRAS',
     negrita: 'EN GARRAPINIADA',
-    imagen: '../../assets/imagenes/dulces_garra.jpg' 
+    imagen: '../../assets/imagenes/productos/dulces_garra.jpg',
+    size:true
   }
 ];
 
